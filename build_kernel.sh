f5f1d0d32b6fef57862a9c1e6cac8f30fe9c9080
Ondrej <36997301+onkofonko@users.noreply.github.com>
2021-09-04 16:57:40 +0200

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,7 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 
-make ARCH=arm64 exynos9810-r7_defconfig
-make ARCH=arm64 -j64
+
+make ARCH=arm64 O=../out mrproper
+make ARCH=arm64 O=../out exynos9810-r7_defconfig
+make ARCH=arm64 O=../out -j$(nproc --all)
