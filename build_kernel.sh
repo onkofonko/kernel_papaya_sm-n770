d8c5bea06b45e862602b4bc95e0689c0b2c4acfc
OnkoFonko <ocky852@gmail.com>
2021-09-15 21:30:39 +0200

    Fix build

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,6 @@ export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
 
-
 make ARCH=arm64 O=../out mrproper
 make ARCH=arm64 O=../out exynos9810-r7_defconfig
 make ARCH=arm64 O=../out -j$(nproc --all)
