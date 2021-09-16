abb10b75b4fdf2b9cb38651156940076638cb5ff
OnkoFonko <ocky852@gmail.com>
2021-09-16 18:00:42 +0200

    Finally build DTB

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,3 +8,7 @@ export ARCH=arm64
 make ARCH=arm64 O=../out mrproper
 make ARCH=arm64 O=../out exynos9810-r7_defconfig
 make ARCH=arm64 O=../out -j$(nproc --all)
+
+python /home/ondrej/kernel/idk/src/mkdtboimg.py create /home/ondrej/kernel/out/arch/arm64/boot/dts/samsung/dtbo.img /home/ondrej/kernel/out/arch/arm64/boot/dts/samsung/*.dtbo
+
+python /home/ondrej/kernel/idk/src/mkdtboimg.py create /home/ondrej/kernel/out/arch/arm64/boot/dts/exynos/dtb.img --id=0 --rev=0 --custom1=0xff000000 /home/ondrej/kernel/out/arch/arm64/boot/dts/exynos/exynos9810.dtb
